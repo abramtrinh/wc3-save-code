502c25452775a9581032a4cfeea6f9fa0de662a3
Abram <abram.trinh@gmail.com>
2023-06-03 16:17:15 -0700

    Added tests for UnHash.

--- breakingHash/breakingHash_test.go
+++ breakingHash/breakingHash_test.go
@@ -1,8 +1,11 @@
 package breakingHash
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/abramtrinh/wc3-save-code/lookup2"
 )
 
 // Keep 10 for iRf74ywBRf to be correct.
@@ -30,4 +33,30 @@ func TestBruteForceLookup2(t *testing.T) {
 	if result != testString {
 		t.Errorf("BruteForceLookup2(%d, 1) = %v; want %v", testHash, result, testString)
 	}
+}
+
+func TestUnHash(t *testing.T) {
+	var tests = []struct {
+		key string
+	}{
+		{""},
+		{" "},
+		{"~"},
+		{"E"},
+		{"AA"},
+		{"Eb"},
+		{"This is the time for all good men to come to the aid of their country"},
+	}
+	for _, test := range tests {
+		testName := fmt.Sprintf("Key: %v", test.key)
+		t.Run(testName, func(t *testing.T) {
+			hash := lookup2.Hash(test.key, 0)
+			unHashString := string(UnHash(hash))
+			unHashReHash := lookup2.Hash(unHashString, 0)
+			if hash != unHashReHash {
+				t.Errorf("%v returned %v, expected %v ", test.key, unHashReHash, hash)
+			}
+		})
+	}
+
 }
